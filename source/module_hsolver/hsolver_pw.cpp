0d17f21ab749f350a8e115efd7f93fa9df7fda9c
dyzheng <zhengdy@aisi.ac.cn>
2026-03-06 15:14:05 +0800

    Fix: cg error of multi-k

--- source/module_hsolver/hsolver_pw.cpp
+++ source/module_hsolver/hsolver_pw.cpp
@@ -682,8 +682,7 @@ void HSolverPW<T, Device>::hamiltSolvePsiK(hamilt::Hamilt<T, Device>* hm,
                                          ct::DataTypeToEnum<Real>::value,
                                          ct::DeviceTypeToEnum<ct::DEVICE_CPU>::value,
                                          ct::TensorShape({static_cast<int>(pre_condition.size())}))
-                               .to_device<ct_Device>()
-                               .slice({0}, {psi.get_current_nbas()});
+                               .to_device<ct_Device>();
 
         cg.diag(hpsi_func, spsi_func, psi_tensor, eigen_tensor, this->ethr_band, prec_tensor);
         // TODO: Double check tensormap's potential problem
@@ -965,8 +964,7 @@ void HSolverPW<T, Device>::fallback_to_cg(hamilt::Hamilt<T, Device>* hm,
                                      ct::DataTypeToEnum<Real>::value,
                                      ct::DeviceTypeToEnum<ct::DEVICE_CPU>::value,
                                      ct::TensorShape({static_cast<int>(pre_condition.size())}))
-                           .to_device<ct_Device>()
-                           .slice({0}, {psi.get_current_nbas()});
+                           .to_device<ct_Device>();
 
     cg.diag(hpsi_func, spsi_func, psi_tensor, eigen_tensor, this->ethr_band, prec_tensor);
     ct::TensorMap(psi.get_pointer(), psi_tensor, {psi.get_nbands(), psi.get_nbasis()}).sync(psi_tensor);
