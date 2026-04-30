1a3ed15d499e4b31ebc094932ba6a1ee014b5033
dyzheng <zhengdy@aisi.ac.cn>
2026-04-30 20:33:35 +0800

    refactor(onsite_proj): extract setup_pw_dftu_indices() from cal_ps_dftu()
    Move one-time DFT+U PW index initialization (orb_l_iat, ip_iat, ip_m,
    vu_begin_iat, vu_device) into a named method setup_pw_dftu_indices().
    Also use ucell->get_npol() instead of implicit npol parameter.

--- source/source_pw/module_pwdft/op_pw_proj.cpp
+++ source/source_pw/module_pwdft/op_pw_proj.cpp
@@ -186,6 +186,69 @@ void OnsiteProj<OperatorPW<T, Device>>::cal_ps_delta_spin(const int npol, const 
 //   nspin=2: tlp1^2 * 1 = tlp1^2 (per spin channel, selected by isk)
 //   nspin=4: tlp1^2 * 4 = (2*tlp1)^2
 template<typename T, typename Device>
+void OnsiteProj<OperatorPW<T, Device>>::setup_pw_dftu_indices() const
+{
+    this->init_dftu = true;
+    auto* onsite_p = projectors::OnsiteProjector<double, Device>::get_instance();
+    const int npol = this->ucell->get_npol();
+
+    resmem_int_op()(this->orb_l_iat, this->ucell->nat);
+    resmem_int_op()(this->ip_m, onsite_p->get_tot_nproj());
+    resmem_int_op()(this->vu_begin_iat, this->ucell->nat);
+    resmem_int_op()(this->ip_iat, onsite_p->get_tot_nproj());
+
+    std::vector<int> ip_iat0(onsite_p->get_tot_nproj());
+    std::vector<int> ip_m0(onsite_p->get_tot_nproj());
+    std::vector<int> vu_begin_iat0(this->ucell->nat);
+    std::vector<int> orb_l_iat0(this->ucell->nat);
+    int ip0 = 0;
+    int vu_begin = 0;
+    for(int iat=0;iat<this->ucell->nat;iat++)
+    {
+        const int it = this->ucell->iat2it[iat];
+        const int target_l = this->dftu->get_orbital_corr(it);
+        orb_l_iat0[iat] = target_l;
+        const int nproj = onsite_p->get_nh(iat);
+        if(target_l == -1)
+        {
+            for(int ip=0;ip<nproj;ip++)
+            {
+                ip_iat0[ip0] = iat;
+                ip_m0[ip0++] = -1;
+            }
+            vu_begin_iat0[iat] = 0;
+            continue;
+        }
+        else
+        {
+            const int tlp1 = 2 * target_l + 1;
+            vu_begin_iat0[iat] = vu_begin;
+            vu_begin += tlp1 * tlp1 * npol * npol;
+            const int m_begin = target_l * target_l;
+            const int m_end  = (target_l + 1) * (target_l + 1);
+            for(int ip=0;ip<nproj;ip++)
+            {
+                ip_iat0[ip0] = iat;
+                if(ip >= m_begin && ip < m_end)
+                {
+                    ip_m0[ip0++] = ip - m_begin;
+                }
+                else
+                {
+                    ip_m0[ip0++] = -1;
+                }
+            }
+        }
+    }
+    syncmem_int_h2d_op()(this->orb_l_iat, orb_l_iat0.data(), this->ucell->nat);
+    syncmem_int_h2d_op()(this->ip_iat, ip_iat0.data(), onsite_p->get_tot_nproj());
+    syncmem_int_h2d_op()(this->ip_m, ip_m0.data(), onsite_p->get_tot_nproj());
+    syncmem_int_h2d_op()(this->vu_begin_iat, vu_begin_iat0.data(), this->ucell->nat);
+
+    resmem_complex_op()(this->vu_device, dftu->get_size_eff_pot_pw());
+}
+
+template<typename T, typename Device>
 void OnsiteProj<OperatorPW<T, Device>>::cal_ps_dftu(
 		const int npol, 
 		const int m) const
@@ -198,8 +261,6 @@ void OnsiteProj<OperatorPW<T, Device>>::cal_ps_dftu(
     auto* onsite_p = projectors::OnsiteProjector<double, Device>::get_instance();
     const std::complex<double>* becp = onsite_p->get_becp();
 
-    // T *ps = new T[tnp * m];
-    // ModuleBase::GlobalFunc::ZEROS(ps, m * tnp);
     if (this->nkb_m < m * tnp) {
         resmem_complex_op()(this->ps, tnp * m, "OnsiteProj<PW>::ps");
         this->nkb_m = m * tnp;
@@ -211,62 +272,7 @@ void OnsiteProj<OperatorPW<T, Device>>::cal_ps_dftu(
 
     if(!this->init_dftu)
     {
-        this->init_dftu = true;
-        //prepare orb_l_iat, ip_m, vu_begin_iat and vu_device
-        resmem_int_op()(this->orb_l_iat, this->ucell->nat);
-        resmem_int_op()(this->ip_m, onsite_p->get_tot_nproj());
-        resmem_int_op()(this->vu_begin_iat, this->ucell->nat);
-        // recal the ip_iat
-        resmem_int_op()(this->ip_iat, onsite_p->get_tot_nproj());
-        std::vector<int> ip_iat0(onsite_p->get_tot_nproj());
-        std::vector<int> ip_m0(onsite_p->get_tot_nproj());
-        std::vector<int> vu_begin_iat0(this->ucell->nat);
-        std::vector<int> orb_l_iat0(this->ucell->nat);
-        int ip0 = 0;
-        int vu_begin = 0;
-        for(int iat=0;iat<this->ucell->nat;iat++)
-        {
-            const int it = this->ucell->iat2it[iat];
-            const int target_l = this->dftu->get_orbital_corr(it);
-            orb_l_iat0[iat] = target_l;
-            const int nproj = onsite_p->get_nh(iat);
-            if(target_l == -1)
-            {
-                for(int ip=0;ip<nproj;ip++)
-                {
-                    ip_iat0[ip0] = iat;
-                    ip_m0[ip0++] = -1;
-                }
-                vu_begin_iat0[iat] = 0;
-                continue;
-            }
-            else
-            {
-                const int tlp1 = 2 * target_l + 1;
-                vu_begin_iat0[iat] = vu_begin;
-                vu_begin += tlp1 * tlp1 * npol * npol;
-                const int m_begin = target_l * target_l;
-                const int m_end  = (target_l + 1) * (target_l + 1);
-                for(int ip=0;ip<nproj;ip++)
-                {
-                    ip_iat0[ip0] = iat;
-                    if(ip >= m_begin && ip < m_end)
-                    {
-                        ip_m0[ip0++] = ip - m_begin;
-                    }
-                    else
-                    {
-                        ip_m0[ip0++] = -1;
-                    }
-                }
-            }
-        }
-        syncmem_int_h2d_op()(this->orb_l_iat, orb_l_iat0.data(), this->ucell->nat);
-        syncmem_int_h2d_op()(this->ip_iat, ip_iat0.data(), onsite_p->get_tot_nproj());
-        syncmem_int_h2d_op()(this->ip_m, ip_m0.data(), onsite_p->get_tot_nproj());
-        syncmem_int_h2d_op()(this->vu_begin_iat, vu_begin_iat0.data(), this->ucell->nat);
-
-        resmem_complex_op()(this->vu_device, dftu->get_size_eff_pot_pw());
+        this->setup_pw_dftu_indices();
     }
 
     const int isk_val = (PARAM.inp.nspin == 2) ? this->isk[this->ik] : 0;
